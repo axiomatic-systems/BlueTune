bbe7825ab29991a15b8284950851dd43223192cb
bok <bok@856e5736-7d0c-0410-af36-8c7e47711be9>
2008-06-07 04:27:30 +0000

    some cleanup

--- Source/Apps/BtMfcGui/stdafx.h
+++ Source/Apps/BtMfcGui/stdafx.h
@@ -40,4 +40,4 @@
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
-#include <afxsock.h>		// MFC socket extensions
+//#include <afxsock.h>		// MFC socket extensions

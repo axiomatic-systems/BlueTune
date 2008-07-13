66c8989cfc2d22c5d1dc5897d3d088ce198b9959
bok <bok@856e5736-7d0c-0410-af36-8c7e47711be9>
2008-07-13 18:37:51 +0000

    Chumby fixes

--- Build/Tools/SCons/gcc-generic.py
+++ Build/Tools/SCons/gcc-generic.py
@@ -1,6 +1,6 @@
 import os
 
-def generate(env, gcc_cross_prefix=None, gcc_strict=True, gcc_stop_on_warning=None):
+def generate(env, gcc_cross_prefix=None, gcc_extra_options='', gcc_strict=True, gcc_stop_on_warning=None):
     ### the environment provides default values for the parameters set to None
     if gcc_stop_on_warning == None: gcc_stop_on_warning = env['stop_on_warning']
     
@@ -24,7 +24,9 @@ def generate(env, gcc_cross_prefix=None, gcc_strict=True, gcc_stop_on_warning=No
     if gcc_cross_prefix:
         env['ENV']['PATH'] += os.environ['PATH']
         env['AR']     = gcc_cross_prefix+'-ar'
+        env['AS']     = gcc_cross_prefix+'-as'
         env['RANLIB'] = gcc_cross_prefix+'-ranlib'
-        env['CC']     = gcc_cross_prefix+'-gcc'
-        env['CXX']    = gcc_cross_prefix+'-g++'
-        env['LINK']   = gcc_cross_prefix+'-g++'
+        env['CPP']    = gcc_cross_prefix+'-cpp'
+        env['CC']     = gcc_cross_prefix+'-gcc ' + gcc_extra_options
+        env['CXX']    = gcc_cross_prefix+'-g++ ' + gcc_extra_options
+        env['LINK']   = gcc_cross_prefix+'-g++ ' + gcc_extra_options
